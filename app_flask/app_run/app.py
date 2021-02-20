de6f9e2743beeea836f44bc97acfccad256bcd18
Polaris-pixel <72374452+Polaris-pixel@users.noreply.github.com>
2021-02-20 12:35:49 -0800

    added js files to run app & csv

--- app_flask/app_run/app.py
+++ app_flask/app_run/app.py
@@ -16,18 +16,18 @@ app = Flask(__name__)
 # Flask Routes
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index_app.html")
 
-@app.route("/datasf")
+@app.route("/get_data")
 def data_sf():
     print('Working')
     #Establish connection with the database
     engine = create_engine('postgresql://postgres:'+ pswd + '@localhost:5432/sf_db')
     connection = engine.connect()
 
-    results = pd.read_sql('select * from sf_data', connection)
+    results = pd.read_sql('SELECT * FROM sf_data LIMIT 20', connection)
     
-    return jsonify((results).to_dict())
+    return jsonify((results).to_dict("record"))
 
 
 
